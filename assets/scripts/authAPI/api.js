661a5ce658e8d8cef7d12791fb404068101adb56
caleybennett <cbennet7@conncoll.edu>
2019-12-12 11:37:17 -0500

    Feature: Sign-in, change password, sign-out
    Created front end for sign-in, change password and sign out.

--- assets/scripts/authAPI/api.js
+++ assets/scripts/authAPI/api.js
@@ -1,6 +1,6 @@
 'use strict'
 const config = require('../config')
-// const store = require('../store')
+const store = require('../store')
 
 const signUp = formData => {
   return $.ajax({
@@ -10,9 +10,38 @@ const signUp = formData => {
   })
 }
 
+const signIn = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const changePassword = formData => {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
+    data: formData
+  })
+}
+
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
+  })
+}
+
 module.exports = {
-  signUp
-  // signIn,
-  // changePassword,
-  // signOut
+  signUp,
+  signIn,
+  changePassword,
+  signOut
 }
--- assets/scripts/authAPI/events.js
+++ assets/scripts/authAPI/events.js
@@ -15,11 +15,42 @@ const onSignUp = function (event) {
     .catch(ui.onSignupFailure)
 }
 
+const onSignIn = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormField(form)
+
+  api.signIn(formData)
+    .then(ui.onSigninSuccess)
+    .catch(ui.onSigninFailure)
+}
+
+const onChangePassword = event => {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormField(form)
+
+  api.changePassword(formData)
+    .then(ui.onChangePasswordSuccess)
+    .catch(ui.onChangePasswordFailure)
+}
+
+const onSignOut = event => {
+  event.preventDefault()
+  // leaving out formData becuase we aren't submitting data...
+  // const form = event.target
+  api.signOut()
+    .then(ui.onSignOutSuccess)
+    .catch(ui.onSignOutFailure)
+}
+
 const addHandlers = event => {
   $('.sign-up').on('submit', onSignUp)
-  // $('#sign-in').on('submit', onSignIn)
-  // $('#change-password').on('submit', onChangePassword)
-  // $('#sign-out').on('submit', onSignOut)
+  $('.sign-in').on('submit', onSignIn)
+  $('.change-password').on('submit', onChangePassword)
+  $('.sign-out').on('submit', onSignOut)
 }
 
 module.exports = {
